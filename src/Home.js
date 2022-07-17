3e5f586386917867a25f36bc51f5f69645da4472
eimaam <imamddahir@gmail.com>
2022-07-18 00:18:45 +0100

    content added to front page

--- src/Home.js
+++ src/Home.js
@@ -40,11 +40,11 @@ function Home(){
                 title="WHy own a website!?"
                 content="According to Forbes Having a website is an opportunity to make a great first impression and give people comfort that you're a real business. With the invention of the Internet, life started a whole new journey with generation of computers getting better. It has grown to be the most preferrable way of communication.
                         With roughly over 60% of the global population using the internet, Websites play an important role as over 70% of small business today own a Website. A website does not only give an easy finding and up to date info about a brand/company but also increases your business' credibility."
-    
+                ImageFirst={true}
                 />
 
                 <Question />
-                
+
                 <Section 
                 className="section-light"
                 image={rocket}
